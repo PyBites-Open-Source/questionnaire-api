ed8d414760fd7379abc0b7f0a772f68a09bd4cb9
Madalin Popa <ppmadalin@icloud.com>
2019-10-02 22:55:38 +0300

    Clean test file

--- tests/test_opentrivia.py
+++ tests/test_opentrivia.py
@@ -25,25 +25,8 @@ class TestModels(unittest.TestCase):
             db.session.remove()
             db.drop_all()
 
-    def test_create_a_question(self):
-        pass
-
-    def test_create_an_answer(self):
-        pass
-
-    def test_delete_question(self):
-        pass
-
 
 class TestWebapp(unittest.TestCase):
-    def setUp(self):
-        # create a test client
-        self.app = create_app("development")
-        # propagate the exceptions to the test client
-        self.app.testing = True
-        # testing client
-        self.client = self.app.test_client()
-
     def setUp(self):
         # create a test client
         self.app = create_app("development")
@@ -66,6 +49,7 @@ class TestWebapp(unittest.TestCase):
         html = response.get_data(as_text=True)
         self.assertIn("<title>Developer Page</title>", html)
 
+
 class TestApiEndpoints(unittest.TestCase):
 
     API_URL = "http://localhost/api/v1/"
@@ -86,21 +70,3 @@ class TestApiEndpoints(unittest.TestCase):
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
-
-    def test_api_create_question(self):
-        pass
-
-    def test_api_get_question(self):
-        pass
-
-    def test_api_get_all_questions(self):
-        pass
-
-    def test_api_add_answer_to_question(self):
-        pass
-
-    def test_api_get_all_answers_of_question(self):
-        pass
-
-    def test_api_get_delete_question(self):
-        pass
