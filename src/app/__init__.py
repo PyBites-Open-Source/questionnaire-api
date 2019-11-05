ae1332c61bc91ce0a3934c09631f8d361949b28c
code-monk08 <mayank.singh081997@gmail.com>
2019-11-06 00:16:04 +0530

    added swagger UI

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -15,13 +15,6 @@ def create_app(config_name):
     # Define app object
     app = Flask(__name__, instance_relative_config=True)
 
-    # Swagger
-    SWAGGER_URL = "/swagger"
-    API_URL = "/static/swagger.json"
-    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-        SWAGGER_URL, API_URL, config={"app_name": "OpenTrivia API"}
-    )
-
     # Load config
     app.config.from_object(app_config[config_name])
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -29,6 +22,11 @@ def create_app(config_name):
     # Init database
     db.init_app(app)  # connects app to db
     migrate = Migrate(app=app, db=db)
+    
+    # Swagger
+    SWAGGER_URL = "/swagger"
+    API_URL = "/static/swagger.json"
+    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={"app_name": "Questionnaire API"})
 
     with app.app_context():
         # Import blueprints
@@ -37,13 +35,14 @@ def create_app(config_name):
         from app.api.routes.questions import question_bp
         from app.api.routes.answers import answer_bp
         from app.api.routes.categories import category_bp
+        API_URL_PREFIX = "/api/v1"
 
         # Register blueprints
         app.register_blueprint(bp_manage)
         app.register_blueprint(webapp)
         app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
-        app.register_blueprint(question_bp, url_prefix="/api/v1")
-        app.register_blueprint(answer_bp, url_prefix="/api/v1")
-        app.register_blueprint(category_bp, url_prefix="/api/v1")
+        app.register_blueprint(question_bp, url_prefix=API_URL_PREFIX)
+        app.register_blueprint(answer_bp, url_prefix=API_URL_PREFIX)
+        app.register_blueprint(category_bp, url_prefix=API_URL_PREFIX)
 
         return app
